841cdcf1c1633fff20718fb60466e469804d64c7
PAPPURAJ <pappuraj.duet@gmail.com>
2023-07-31 13:26:40 +0600

    init

---
+++ src/main/java/com/github/pappuraj/ecoaching/ECoachingApplication.java
@@ -0,0 +1,13 @@
+package com.github.pappuraj.ecoaching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECoachingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECoachingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/pappuraj/ecoaching/ECoachingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.pappuraj.ecoaching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECoachingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
